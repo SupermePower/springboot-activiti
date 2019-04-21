1460bae2b29b0d2f39a4f0f68b33324efa2a7951
TalentFly <1483118244@qq.com>
2019-04-21 23:59:17 +0800

    完善流程

--- user-server/src/test/java/com/nb/user/UserServerApplicationTests.java
+++ user-server/src/test/java/com/nb/user/UserServerApplicationTests.java
@@ -1,6 +1,11 @@
 package com.nb.user;
 
 import com.baomidou.mybatisplus.toolkit.IdWorker;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,4 +21,23 @@ public class UserServerApplicationTests {
         System.out.println(id);
     }
 
+    /**
+     * 测试发布流程
+     */
+    @Test
+    public void publishProcessTest() {
+        // 核心对象（服务大管家）
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //获取repositoryService仓库服务
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        //获取流程发布对象
+        DeploymentBuilder createDeployment = repositoryService.createDeployment();
+        //上传资源
+        createDeployment.addClasspathResource("processes/StallProcessChoose.bpmn");
+//        createDeployment.addClasspathResource("StallProcessChoose.png");
+        //发布
+        Deployment deploy = createDeployment.deploy();
+        //DeploymentEntity[id=1, name=null]
+        System.out.println(deploy);
+    }
 }
